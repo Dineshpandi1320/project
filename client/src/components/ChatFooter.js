7b20dff6137017054008ff79f5e10892adf737c8
Dineshpandi1320 <dineshd1307@gmail.com>
2023-05-03 13:41:16 +0530

    chat app

---
+++ client/src/components/ChatFooter.js
@@ -0,0 +1,17 @@
+
+import Status from './Status';
+
+function ChatFooter() {
+  return (
+    <footer className="columns">
+      <div className="column is-hidden-mobile has-text-left">
+        <small>&copy; {new Date().getFullYear()} - Dinesh  (<a href="" target="_blank" rel="noreferrer" className="has-text-white"></a>)</small>
+      </div>
+      <div className="column has-text-right-tablet has-text-centered">
+        <Status />
+      </div>
+    </footer>
+  );
+}
+
+export default ChatFooter;
